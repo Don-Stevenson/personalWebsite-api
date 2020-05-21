0c1fdb08b074020c66392712506a8f45ab6bf61a
Don-Stevenson <dsteven1@gmail.com>
2020-05-21 00:16:16 -0400

    set up headers to be sent back to us

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("bodyParser");
 const cors = require("cors");
-const sendGrid = require('@sendgrid/mail')
-const app = express()
+const sendGrid = require("@sendgrid/mail");
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((request, response, next) => {
+  request.setHeader("Acess-Control-Allow-Origin", "*");
+  request.setHeader(
+    "Acess-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  request.setHeader(
+    "Acess-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+});
