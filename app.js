eeca65e46dcb4d7edf6076c5391782c8a0dc2e0d
Don-Stevenson <dsteven1@gmail.com>
2020-05-21 00:28:28 -0400

    server working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("bodyParser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const sendGrid = require("@sendgrid/mail");
 
@@ -9,13 +9,21 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use((request, response, next) => {
-  request.setHeader("Acess-Control-Allow-Origin", "*");
-  request.setHeader(
+  response.setHeader("Acess-Control-Allow-Origin", "*");
+  response.setHeader(
     "Acess-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
   );
-  request.setHeader(
+  response.setHeader(
     "Acess-Control-Allow-Headers",
     "Content-Type, Authorization"
   );
+  next();
 });
+
+const PORT = 3030;
+// to check server is working
+app.get("/api", (request, response, next) => {
+  response.send(`API Status: Running on ${PORT}`);
+});
+app.listen(PORT, "0.0.0.0");
