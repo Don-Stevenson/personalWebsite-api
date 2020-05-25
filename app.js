99dfe9744a63ad38ee4c618e3044365e554b154f
Don-Stevenson <dsteven1@gmail.com>
2020-05-24 20:55:47 -0400

    cleanup code and add comments

--- app.js
+++ app.js
@@ -16,32 +16,16 @@ app.use(bodyParser.json());
 app.use(Cors());
 
 // to check server is working
+//************************* */
 app.get("/api", (request, response, next) => {
   response.send(`API Status: Running on ${PORT}`);
 });
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`App listening on port ${PORT}!`);
 });
-// testing out sgMail
-// const msg = {
-//   to: "dsteven1@gmail.com",
-//   from: request.body.email,
-//   subject: "Contact from website",
-//   text: request.body.message
-// };
 
-// async function sendMessage() {
-//   try {
-//     console.log("api key is", process.env.SENDGRID_API_KEY);
-//     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-//     await sgMail.send(msg);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// console.log(sendMessage());
-
+// configure app
+//*************** */
 app.use((request, response, next) => {
   response.setHeader("Acess-Control-Allow-Origin", "*");
   response.setHeader(
