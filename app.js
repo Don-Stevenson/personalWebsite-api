b4e8f2619b4f388721d944d172b90be08de7ca5e
Don Stevenson <dstevenson@tribalscale.com>
2023-03-29 19:28:09 -0400

    Update formatting to deploy

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const Cors = require("cors");
-const sgMail = require("@sendgrid/mail");
-const PORT = 3030;
-const dotenv = require("dotenv");
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const Cors = require("cors")
+const sgMail = require("@sendgrid/mail")
+const PORT = 3030
+const dotenv = require("dotenv")
 // setup dotenv options
 // ********************
 dotenv.config({
   path: "./.env",
-});
+})
 // app configurations
 // ******************
-app.use(bodyParser.json());
-app.use(Cors());
+app.use(bodyParser.json())
+app.use(Cors())
 // to check that the server is working
 // ***********************************
 app.get("/api", (request, response, next) => {
-  response.send(`API Status: Running on ${PORT}`);
-});
+  response.send(`API Status: Running on ${PORT}`)
+})
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+  console.log(`App listening on port ${PORT}!`)
+})
 // configure app
 // *************
 app.use((request, response, next) => {
-  response.setHeader("Acess-Control-Allow-Origin", "*");
+  response.setHeader("Acess-Control-Allow-Origin", "*")
   response.setHeader(
     "Acess-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
-  );
+  )
   response.setHeader(
     "Acess-Control-Allow-Headers",
     "Content-Type, Authorization"
-  );
-  next();
-});
+  )
+  next()
+})
 app.post("/api/email", (request, response, next) => {
   // sets the apikey
   // ***************
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY2);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY2)
   // bring in my email from the.env file
   // ***********************************
-  const myEmail = process.env.MY_EMAIL;
+  const myEmail = process.env.MY_EMAIL
   // message setup from the input fields of website- so as not error out twillio
   // make the default email my own, put the users email in the subject and in the
   // text as reply to
@@ -56,26 +56,26 @@ app.post("/api/email", (request, response, next) => {
     message: ${request.body.message}
      
     reply to: ${request.body.email}`,
-  };
+  }
   // sending the email and catching any errors
   // *****************************************
   // updated with async await
   const sendMessage = async () => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY2);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY2)
     try {
-      await sgMail.send(msg);
+      await sgMail.send(msg)
       response.status(200).json({
         success: true,
-      });
+      })
     } catch (error) {
-      console.error("error is: ", error);
+      console.error("error is: ", error)
       response.status(401).json({
         success: false,
-      });
+      })
     }
-  };
+  }
   // Console log the message for development purposes
   // ************************************************
-  sendMessage();
-  console.log("Message is : ", msg);
-});
+  sendMessage()
+  console.log("Message is : ", msg)
+})
