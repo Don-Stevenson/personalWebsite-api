a5e51240b1e1f291b5a29975b4252b457c27bc46
Don Stevenson <dstevenson@tribalscale.com>
2024-12-08 16:32:48 -0500

    Update Api key variable name

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const bodyParser = require("body-parser")
-const Cors = require("cors")
-const sgMail = require("@sendgrid/mail")
+const bodyParser = require('body-parser')
+const Cors = require('cors')
+const sgMail = require('@sendgrid/mail')
 const PORT = 3030
-const dotenv = require("dotenv")
+const dotenv = require('dotenv')
 // setup dotenv options
 // ********************
 dotenv.config({
-  path: "./.env",
+  path: './.env',
 })
 // app configurations
 // ******************
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(Cors())
 // to check that the server is working
 // ***********************************
-app.get("/api", (request, response, next) => {
+app.get('/api', (request, response, next) => {
   response.send(`API Status: Running on ${PORT}`)
 })
 app.listen(process.env.PORT || 3000, () => {
@@ -25,21 +25,21 @@ app.listen(process.env.PORT || 3000, () => {
 // configure app
 // *************
 app.use((request, response, next) => {
-  response.setHeader("Acess-Control-Allow-Origin", "*")
+  response.setHeader('Acess-Control-Allow-Origin', '*')
   response.setHeader(
-    "Acess-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
+    'Acess-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
   )
   response.setHeader(
-    "Acess-Control-Allow-Headers",
-    "Content-Type, Authorization"
+    'Acess-Control-Allow-Headers',
+    'Content-Type, Authorization'
   )
   next()
 })
-app.post("/api/email", (request, response, next) => {
+app.post('/api/email', (request, response, next) => {
   // sets the apikey
   // ***************
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY2)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   // bring in my email from the.env file
   // ***********************************
   const myEmail = process.env.MY_EMAIL
@@ -50,25 +50,31 @@ app.post("/api/email", (request, response, next) => {
   const msg = {
     to: myEmail,
     from: myEmail,
-    subject: `donstevenson.netlify.app email from ${request.body.name}`,
-    text: `    message from: ${request.body.name}
+    subject: `donstevenson.netlify.app email from ${
+      request.body.name ? request.body.name : 'dsteven1@gmail.com'
+    }`,
+    text: `    message from: ${
+      request.body.name ? request.body.name : 'dsteven1@gmail.com'
+    }}
 
-    message: ${request.body.message}
-     
-    reply to: ${request.body.email}`,
+    message: ${request.body.message ? request.body.message : 'test'}
+
+    reply to: ${
+      request.body.email ? request.body.email : 'dsteven1@gmail.com'
+    }`,
   }
   // sending the email and catching any errors
   // *****************************************
   // updated with async await
   const sendMessage = async () => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY2)
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     try {
       await sgMail.send(msg)
       response.status(200).json({
         success: true,
       })
     } catch (error) {
-      console.error("error is: ", error)
+      console.error('error is: ', error)
       response.status(401).json({
         success: false,
       })
@@ -77,5 +83,5 @@ app.post("/api/email", (request, response, next) => {
   // Console log the message for development purposes
   // ************************************************
   sendMessage()
-  console.log("Message is : ", msg)
+  console.log('Message is : ', msg)
 })
